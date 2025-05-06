7075fc642de403489afcfdf1cd982af921a0fa8d
Tim Geenen <timmiej_95@hotmail.com>
2025-05-06 13:28:01 +0900

    feat: added centralized import file to global components folder

---
+++ apps/client/src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Input } from './ui/Input';
+export { default as Label } from './ui/Label';
+export { default as Button } from './ui/Button';
+export { default as NavButton } from './ui/NavButton';
