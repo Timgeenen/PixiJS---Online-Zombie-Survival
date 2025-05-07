ed0d6bc8c0763324316c3703f28751f12fe0544f
Tim Geenen <timmiej_95@hotmail.com>
2025-05-07 15:59:55 +0900

    feat: added base functions for api calls

--- apps/client/src/features/authentication/services/authServices.ts
+++ apps/client/src/features/authentication/services/authServices.ts
@@ -1,11 +1,17 @@
 import { HTTPClient } from '@API';
-
-type Credentials = {
-    username: string;
-    password: string;
-};
+import type { Credentials } from '../types';
 
 export async function login(credentials: Credentials) {
     const response = await HTTPClient.post('/api/auth/login', credentials);
+    console.log(response);
+}
+
+export async function guestLogin() {
+    const response = await HTTPClient.get('/api/auth/guest');
+    console.log(response);
+}
+
+export async function register(credentials: Credentials) {
+    const response = await HTTPClient.post('/api/auth/register', credentials);
     console.log(response);
 }
