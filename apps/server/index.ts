ffbc803086f96c3e52fc3d8d9a767838e0178cb9
Tim Geenen <timmiej_95@hotmail.com>
2025-05-08 17:44:56 +0900

    feat: added custom error handling

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,4 +1,5 @@
 import connectDB from '@Config/db';
+import { errorHandler } from '@Middleware/errorMiddleware';
 import router from '@Routes/index';
 
 const dotenv = require('dotenv');
@@ -14,6 +15,24 @@ app.use(cors());
 app.use('/api', router);
 
 connectDB();
+
+app.use(errorHandler);
+
+//Handle uncaught errors
+process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception:', err.message);
+    process.exit(1);
+});
+
+// Handle unhandled promise rejections (async errors outside Express)
+process.on('unhandledRejection', (err) => {
+    if (err instanceof Error) {
+        console.error('Unhandled Promise Rejection:', err.message);
+    } else {
+        console.error('Unhandled Promise Rejection (not instanceof Error): ', err);
+    }
+    process.exit(1);
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`server listening on ${process.env.PORT}`);
