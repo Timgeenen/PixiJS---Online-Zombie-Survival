1d03b3ecc34e7487dc3081a86c3d0c1165e44590
Tim Geenen <timmiej_95@hotmail.com>
2025-05-17 13:19:48 +0900

    refactor: updated login route to add jwt tokens to cookies

--- apps/server/controllers/authController.ts
+++ apps/server/controllers/authController.ts
@@ -1,23 +1,35 @@
 import { sendSuccess } from '@Utils/apiResponse';
 import { comparePassword, hashPassword } from '@Utils/hash';
-import { AuthError, BadRequestError, ConflictError, InternalServerError, NotFoundError } from 'errors/customErrors';
-import type { Request, Response } from 'express';
-import { createNewUser, findUserByUsername, isEmailTaken, isUsernameTaken } from 'services/userService';
+import {
+    AuthError,
+    BadRequestError,
+    ConflictError,
+    InternalServerError,
+    NotFoundError,
+} from 'errors/customErrors';
+import type { NextFunction, Request, Response } from 'express';
+import {
+    createNewUser,
+    findUserByUsername,
+    isEmailTaken,
+    isUsernameTaken,
+} from 'services/userService';
+import { setNewToken } from '@Utils/jwt';
 
-const jwt = require('jsonwebtoken');
-
-exports.login = async (req: Request, res: Response) => {
+export async function login(req: Request, res: Response, next: NextFunction) {
     try {
         const { username, password } = req.body;
-    
+
         if (!username || !password) {
-            throw new BadRequestError(`Missing credentials: ${!username && "username, "}${!password && "password"}`);
+            throw new BadRequestError(
+                `Missing credentials: ${!username && 'username, '}${!password && 'password'}`,
+            );
         }
 
         const user = await findUserByUsername(username);
-    
+
         if (!user) {
-            throw new NotFoundError("User not found");
+            throw new NotFoundError('User not found');
         }
 
         const match = await comparePassword(password, user.password);
@@ -26,13 +38,17 @@ exports.login = async (req: Request, res: Response) => {
             throw new AuthError('Invalid password');
         }
 
-        sendSuccess(res, user, 201, "Log in success");
-    } catch (error) {
-        throw new InternalServerError('Error occurred while trying to log in')
-    }
-};
+        const userID = user._id.toString();
+        setNewToken(res, userID, 'refresh');
+        setNewToken(res, userID, 'access');
 
-exports.register = async (req: Request, res: Response) => {
+        sendSuccess(res, user, 201, 'Log in success');
+    } catch (error) {
+        next(error);
+    }
+}
+
+export async function register(req: Request, res: Response, next: NextFunction) {
     try {
         const { username, email, password } = req.body;
 
@@ -62,30 +78,17 @@ exports.register = async (req: Request, res: Response) => {
             password: hashedPassword,
         });
 
-        sendSuccess(res, user, 201, "Successfuly created user");
+        sendSuccess(res, user, 201, 'Successfuly created user');
     } catch (error) {
-        throw new InternalServerError('Error in authController.register');
+        next(error);
     }
-};
+}
 
-exports.guestLogin = (req: Request, res: Response) => {
+export async function guestLogin(req: Request, res: Response, next: NextFunction) {
     res.json({
         user: {
             id: 420,
             username: 'guest',
         },
     });
-};
-
-// exports.validateToken = (req: Request, res: Response) => {
-//     const token = req.header('Authorization')?.replace('Bearer ', '');
-//     if (!token) {
-//         res.status(401).json({ error: 'No token provided' });
-//     }
-//     try {
-//         const decoded = jwt.validateToken(token, process.env.JWT_SECRET);
-//         res.json({ user: { id: decoded.id, username: decoded.username } });
-//     } catch (err) {
-//         res.status(401).json({ error: 'Invalid token' });
-//     }
-// };
+}
