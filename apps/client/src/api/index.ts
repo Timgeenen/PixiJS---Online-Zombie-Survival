89f20947dab52b6d7e272b81fa55411df805c4ee
Tim Geenen <timmiej_95@hotmail.com>
2025-05-17 13:24:01 +0900

    feat: added condition to configure setup based on environment

--- apps/client/src/api/index.ts
+++ apps/client/src/api/index.ts
@@ -1,3 +1,7 @@
 import { AxiosHTTPClient } from '@Library/axios';
 
-export const HTTPClient = new AxiosHTTPClient(import.meta.env.VITE_BACKEND_URL);
+export const HTTPClient = new AxiosHTTPClient(
+    import.meta.env.VITE_ENV === 'production'
+        ? import.meta.env.VITE_BACKEND_URL
+        : import.meta.env.VITE_LOCALHOST,
+);
