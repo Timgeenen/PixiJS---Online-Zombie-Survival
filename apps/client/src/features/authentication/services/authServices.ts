0c205feaf0bda1bad2dfedb51cce8c9cc23b24a7
Tim Geenen <timmiej_95@hotmail.com>
2025-05-18 16:00:44 +0900

    feat: added explicit return types for auth services

--- apps/client/src/features/authentication/services/authServices.ts
+++ apps/client/src/features/authentication/services/authServices.ts
@@ -1,17 +1,18 @@
 import { HTTPClient } from '@API';
-import type { Credentials } from '@monorepo/shared';
+import type { ApiResponse, Credentials } from '@monorepo/shared';
+import type { User } from '@Types';
 
-export async function login(credentials: Credentials) {
-    const response = await HTTPClient.post('/api/auth/login', credentials);
-    console.log(response);
+export async function login(credentials: Credentials): Promise<ApiResponse<User>> {
+    const response: ApiResponse<User> = await HTTPClient.post('/api/auth/login', credentials);
+    return response;
 }
 
 export async function guestLogin() {
-    const response = await HTTPClient.get('/api/auth/guest');
-    console.log(response);
+    const response: ApiResponse<User> = await HTTPClient.get('/api/auth/guest');
+    return response;
 }
 
 export async function register(credentials: Credentials) {
-    const response = await HTTPClient.post('/api/auth/register', credentials);
-    console.log(response);
+    const response: ApiResponse<User> = await HTTPClient.post('/api/auth/register', credentials);
+    return response;
 }
