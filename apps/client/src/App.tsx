d9c7c0eff04ae2dd14c95f537b61d1020dcad415
Tim Geenen <timmiej_95@hotmail.com>
2025-05-18 16:04:19 +0900

    feat: added queryclientprovider

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,11 +1,19 @@
 import { AppRouter } from '@Routes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter as Router } from 'react-router';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 function App() {
+    const nodeEnv = import.meta.env.VITE_ENV;
     return (
-        <Router>
-            <AppRouter />
-        </Router>
+        <QueryClientProvider client={queryClient}>
+            <Router>
+                <AppRouter />
+            </Router>
+            {nodeEnv === 'dev' && <ReactQueryDevtools />}
+        </QueryClientProvider>
     );
 }
 
