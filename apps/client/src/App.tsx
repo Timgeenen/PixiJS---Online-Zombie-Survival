ce39f5828dcc65a5d3882e92a348404034df86ed
Tim Geenen <timmiej_95@hotmail.com>
2025-05-23 17:16:29 +0900

    feat: added debugger for socket in dev env

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,17 +1,16 @@
 import { AppRouter } from '@Routes';
-import { authorizeUser } from '@Services';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router';
+
+const nodeEnv = import.meta.env.VITE_ENV;
+if (nodeEnv === 'dev') {
+    localStorage.setItem('debug', 'socket.io-client:socket');
+}
 
 const queryClient = new QueryClient();
 
 function App() {
-    const nodeEnv = import.meta.env.VITE_ENV;
-    useEffect(() => {
-        authorizeUser();
-    }, []);
     return (
         <QueryClientProvider client={queryClient}>
             <Router>
